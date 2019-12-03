bb5ff6bc5ecc3d7d3d45c243a2f40159c46984ae
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-12-02 18:13:28 -0700

    lists and tuples

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,43 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE 
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
+x.extend(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE 
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
+x.insert(5,99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE 
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+
+# This multiplied all of them together...not what I want
+# def multiply(list):
+#     num = 1000
+#     for x in list:
+#         result = num * x
+#     return result
+
+# print(multiply(x))
+
+new_x = []
+
+for i in x:
+    new_x.append(i * 1000)
+
+print(new_x)
